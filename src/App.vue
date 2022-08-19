62efd8ae0076125b4c3546cd76e76816d47d646d
高江华 <g598670138@163.com>
2022-08-19 16:27:02 +0800

    style(custom): 配置代码格式工具

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
-import useStore from './store';
+import useStore from './store'
 </script>
 
 <template>
     <!-- 一级路由出口 -->
     <div class="h-screen fixed top-0 left-0 w-screen">
         <!-- <router-view></router-view> -->
-        <g-transition-router-view mainComponentName="home" :routerType="useStore().common.routerType">
+        <g-transition-router-view
+            mainComponentName="home"
+            :routerType="useStore().common.routerType"
+        >
         </g-transition-router-view>
     </div>
 </template>
